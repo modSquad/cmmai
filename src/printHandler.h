a9b576219dd56403509554c13a3128172a8a3dc4
Martin,Richard <martius@martiusweb.net>
2010-12-01 13:08:14 +0100

    headers server 2/2

---
+++ src/printHandler.h
@@ -0,0 +1,13 @@
+/* CIAI : Développement Multi-lots
+ * @author H4203
+ *
+ * Task managing the printing of label on boxes.
+ */
+
+#ifndef PRINT_HANDLER_H
+#define PRINT_HANDLER_H
+
+#include "boxingServer.h"
+
+#endif
+
