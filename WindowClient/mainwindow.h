50d461bf31c52c614296761edf8dd155da780270
Etienne Brodu <etienne.brodu@insa-lyon.fr>
2010-12-06 09:48:34 +0100

    comment windowClient

--- WindowClient/mainwindow.h
+++ WindowClient/mainwindow.h
@@ -18,14 +18,41 @@ public:
     ~MainWindow();
 
 public slots:
+    /** addAccepted(int number)
+      * Called when a pieces is boxed to make changes to the interface
+      */
     void addAccepted(int number);
+
+    /** addRejected(int number)
+      * Called when a pieces is rejected to make changes to the interface
+      */
     void addRejected(int number);
+
+    /** appendLog(QString logLine)
+      * Called to apped a line to the log.
+      */
     void appendLog(QString logLine);
 
+
+    /** sendConfig()
+      * Called by interface to send configuration data to the server
+      */
     void sendConfig();
 
+
+    /** connectToHost()
+      * Called by interface to connect to the server specified in the address text line.
+      */
     void connectToHost();
+
+    /** connectionNotify()
+      * Called to notify when the socket is connected
+      */
     void connectionNotify();
+
+    /** disconnectionNotify()
+      * Called to notify when the socket is disconnected
+      */
     void disconnectionNotify();
 
 private:
