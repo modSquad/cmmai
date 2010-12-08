721ccb6cb817bf068cb29bb45697c4935e758eb9
unknown <mgolumbean@.insa-lyon.fr>
2010-12-08 09:44:12 +0100

    interface devices

--- src/devices.h
+++ src/devices.h
@@ -30,20 +30,20 @@ typedef enum {
 } color_t;
 
 /* Interface for the valves  - InletValve, OutletValve*/
-void closeValve(valveName_t valveName); /*  closes the valve */
-void openValve(valveName_t valveName); /*  opens the valve */
-bool valveStatus (valveName_t valveName); /* returns the state of the valve - opened or closed */
+void setValveState(valveName_t valveName, BOOL valveState);
+BOOL valveStatus (valveName_t valveName); /* returns the state of the valve - opened or closed */
 
 /* Interface for the sensors */
-bool presenceDetected(sensorName_t sensorName); /* returns whether the sensor detected presence or not */
-bool defectedProduct(sensorName_t sensorName); /* detects whether the product that passed in front of the sensor is defected or not */
+BOOL presenceDetected(sensorName_t sensorName); /* returns whether the sensor detected presence or not */
+BOOL defectedProduct(sensorName_t sensorName); /* detects whether the product that passed in front of the sensor is defected or not */
 
 /* Interface for the printers */
-bool printerState(printerName_t printerName); /* returns whether a printer is ready to print or not */
+void setPrinterState(printerName_t printerName, BOOL printerState) /* sets the state of a printer - ready to print or not */
+BOOL printerState(printerName_t printerName); /* returns whether a printer is ready to print or not */
 void print(printerName_t printerName, boxData_t boxData); /* sends the print command to the printer, the printer is unavailable while printing */
 
 /* Interface for the lights */
 void setColor(color_t color); /* Sets the color of the light device*/
-
+color_t getColor();
 
 #endif
