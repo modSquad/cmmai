2ac33db4bce27c1650675433abd839144bf85a0d
Etienne Brodu <etienne.brodu@insa-lyon.fr>
2010-12-08 09:51:20 +0100

    eventManager + setLight

--- src/eventManager.c
+++ src/eventManager.c
@@ -1,20 +1,98 @@
 #include "eventManager.h"
 #include "msgQLib.h"
+#include "taskLib.h"
 
-void eventManager (int socketInput, MSG_Q_ID eventsQueue)
+void setLight(event_t event)
 {
-	eventStruct_t eventStruct;
+
+/* THIS METHOD HAS NO WAY TO WORK PROPERLY
+ * because any of "Red" or "Orange" event could be set as resolved.
+ */
+
+	static lastPrimaryEvent = EVT_NONE;
+	static lastSecondaryEvent = EVT_NONE; 
+	
+	if(event == EVT_NONE)
+	{
+		setColor(GREEN);
+	}
+
+	//Red :
+	if(event == EVT_EMERGENCY_STOP
+	|| event == EVT_ERR_FULL_QUEUE
+	|| event == EVT_ERR_PRODUCT_STARVATION
+	|| event == EVT_ERR_BOX_STARVATION
+	|| event == EVT_ERR_DEFECTIVE_TRESHOLD_REACHED )
+	{
+		setColor(RED);
+		lastPrimaryEvent = event;
+	}
+
+	//Orange :
+	if(event == EVT_ANOMALY_PRINTER1
+	|| event == EVT_ANOMALY_PRINTER2 )
+	{
+		if(lastPrimaryEvent == EVT_NONE)
+		{
+			setColor(ORANGE)
+		}
+		else
+		{
+			lastSecondaryEvent == event;
+		}
+	}
+
+	//Green :
+	if(event == EVT_END_FILLING
+	|| event == EVT_CLOSE_APPLICATION
+	|| event == EVT_BOX_PROCESSED
+	|| event == EVT_BOX_PRINTED )
+	{
+		if(lastPrimaryEvent == EVT_NONE)
+		{
+			if(lastSecondaryEvent == EVT_NONE)
+			{
+				setColor(GREEN);
+			}
+			else
+			{
+				setColor(ORANGE);
+			}
+		}
+	}
+}
+
+void eventManager (int socketInput, MSG_Q_ID eventsQueue, MSG_Q_ID logsEventQueue)
+{
+	event_msg_t eventMsg;
+	char[MIN_EVENT_STRING_SIZE] logLine;
+	static event_t lastEvent;
 	
 	for(;;)
 	{
-		if( msgQReceive(eventsQueue, eventBuffer, sizeof(eventStruct_t), WAIT_FOREVER) == ERROR )
+		if( msgQReceive(eventsQueue, eventMsg, sizeof(event_msg_t), WAIT_FOREVER) == ERROR )
 		{
-			// Receive message fail
+			continue;
 		}
 
-		
-		
+		if(msgQSend(logsEventQueue, eventMsg, sizeof(event_msg_t), WAIT_FOREVER ) == ERROR )
+		{
+			// send to log error
+		}
 
+		eventToString (eventMsg.event, eventMsg.boxData, logLine);
+		send( socketInput, logLine, MIN_EVENT_STRING_SIZE, 0);
+
+		if( eventMsg.event != EVT_NONE )
+		{
+			setLight(eventMsg.event);
+			lastEvent = eventMsg.event;
+		}
+		else
+		{
+			setLight(eventMsg.event);
+			taskSuspend(taskIdSelf());
+		}
 		
 	}
 }
