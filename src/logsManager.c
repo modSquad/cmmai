93cbd3531d5f1cdc2d131d86da121ad4d28baefe
Maxime Gaudin <gaudin.maxime@gmail.com>
2010-12-14 19:19:17 +0100

    Qjout d4un truc pour syncrhoniser lq fin de l4qpplicqtion< Putqin de clqvie de auebeauois de ;erde///

--- src/logsManager.c
+++ src/logsManager.c
@@ -31,11 +31,13 @@ int logsManager(MSG_Q_ID logsEventsQueue, SEM_ID endSync)
 		}
 		else
 		{
-			eventToString(event_buffer.event, &event_buffer.boxData, logEntry);
+			if(event_buffer.event == EVT_CLOSE_APPLICATION)
+				semGive(endSync);
+			eventToString(event_buffer.event, &event_buffer.boxData, logEntry); 
 			write(log_fd, logEntry, strlen(logEntry));
 			/* We must ensure that the data have been written to the
-			 * disk : in case of critical error, this is the only
-			 * way to know what happened in the system. */
+			* disk : in case of critical error, this is the only
+			* way to know what happened in the system. */
 			/* TODO : Find VxWorks equivalent for fflush */
 			/* fflush(log_fd); */
 		}	
